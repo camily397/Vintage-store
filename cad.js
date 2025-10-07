a7c24f7f912af9f49f849de6cc1c39ba81dc2ac9
camily397 <camily.itena@escola.pr.gov.br>
2025-10-07 08:39:51 -0300

    cad.js

---
+++ cad.js
@@ -0,0 +1,10 @@
+  <script>
+    document.getElementById("cadastroForm").addEventListener("submit", function(e) {
+      e.preventDefault();
+      const nome = document.getElementById("nome").value;
+      alert(`Conta criada com sucesso!\nBem-vindo(a), ${nome}!`);
+      window.location.href = "login.html";
+    });
+  </script>
+</body>
+</html>
