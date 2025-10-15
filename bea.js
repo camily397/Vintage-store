47b8efeab89f5b5e74bc6365517ec6f3e5ab7475
camily397 <camily.itena@escola.pr.gov.br>
2025-10-15 12:09:54 -0300

    bea.js

--- bea.js
+++ bea.js
@@ -62,3 +62,17 @@ cartBtn.addEventListener("click", () => {
     cartBtn.classList.remove("added");
   }, 2000);
 });
+// ======= Adicionar ao carrinho =======
+const cartBtn = document.getElementById("addToCart");
+
+cartBtn.addEventListener("click", () => {
+  // muda o texto e a cor
+  cartBtn.textContent = "✔️ Adicionado!";
+  cartBtn.classList.add("added");
+
+  // volta ao normal depois de 2 segundos
+  setTimeout(() => {
+    cartBtn.textContent = "🛒 Adicionar ao carrinho";
+    cartBtn.classList.remove("added");
+  }, 2000);
+});
