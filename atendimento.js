4aecbf3a436ecc5d2cdadb9c8f7bab67cbb2319e
bortsagi <giulia.borth.santos@escola.pr.gov.br>
2025-10-16 21:11:36 -0300

    Update atendimento.js

--- atendimento.js
+++ atendimento.js
@@ -1,4 +1,3 @@
-// JS antigo do Atendimento
-console.log("Atendimento.js carregado corretamente");
-
-// Futuras funcionalidades do menu podem ser adicionadas aqui
+function irPara(pagina) {
+  window.location.href = pagina;
+}
