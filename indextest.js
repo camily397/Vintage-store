84fe723a4fc3effdbf9a72d06c447c2715edb6c8
vini <vinikasantos@gmail.com>
2025-10-21 11:40:24 -0300

    Update indextest.js

--- indextest.js
+++ indextest.js
@@ -2,7 +2,7 @@ import supabase from './supabaseClient.js';
 
 async function buscarDados() {
   const { data, error } = await supabase
-    .from('nome_da_sua_tabela')  // Troca pelo nome da sua tabela no Supabase
+    .from('usuarios')  // Troca pelo nome da sua tabela no Supabase
     .select('*');  // Seleciona todas as colunas
 
   if (error) {
