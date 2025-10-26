2a5d7dce9cb24139181b6bddbf6c88803abe2253
bortsagi <giulia.borth.santos@escola.pr.gov.br>
2025-10-26 19:12:55 -0300

    Update yves.js

--- yves.js
+++ yves.js
@@ -15,7 +15,7 @@ const qtyDisplay = document.getElementById("qty");
 const totalDisplay = document.getElementById("total");
 
 let quantity = 1;
-const price = 119.90; // preço unitário para Beatopia
+const price = 225.90; // preço unitário para Beatopia
 
 function updateTotal() {
   totalDisplay.textContent = (price * quantity).toFixed(2).replace(".", ",");
@@ -84,7 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const product = {
       id: "yves-loop",
       name: "Yves - Loop",
-      price: 99.90,
+      price: 225.90,
       quantity: parseInt(qtyElement.textContent),
       image: "loop.jfif"
     };
