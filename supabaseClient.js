3ec18ac9cbc9245609592a7ff249dfe97044f941
camily397 <camily.itena@escola.pr.gov.br>
2025-10-26 13:18:13 -0300

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  'https://qgacdbbfpkvunlkofckt.supabase.co', 
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnYWNkYmJmcGt2dW5sa29mY2t0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2MjI1NDMsImV4cCI6MjA3NDE5ODU0M30.8Sj3XFkd49olF6997K9iciuVboYdKIBjDWzzo7w5xp8'
-);
-
-export default supabase;
